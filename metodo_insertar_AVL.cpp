07d31f41bf618ffe67dac37ee953ffabea210aec
Alex253 <alex08lta@gmail.com>
2018-06-07 00:36:30 -0300

    agregados altura y 4 casos

--- metodo_insertar_AVL.cpp
+++ metodo_insertar_AVL.cpp
@@ -1,3 +1,4 @@
+//CREAR METODO setRaiz(Nodo);
 crearAVL(){
 	rama = new Nodo();
 }
@@ -24,4 +25,69 @@ void insertarPalabra(string palabra,Nodo * raiz){
 			insertarPalabra(palabra,raiz->getHijoDerecho());
 		}
 	}
+}
+
+int Nodo::altura(){
+	if(this->getHijoDerecho()!=NULL && this->getHijoIzquierdo()!=NULL){
+		int de = this->getHijoDerecho()->altura();
+		int iz = this->getHijoIzquierdo()->altura();
+		return 1+(de >= iz?de:iz); 
+	}
+	else if(this->getHijoDerecho()!=NULL){
+		return 1+this->getHijoDerecho()->altura();	
+	}
+	else if(this->getHijoIzquierda()!=NULL){
+		return 1+this->getHijoDerecho()->altura();	
+	}
+	else{
+		return 0;
+	}
+}
+
+void caso1 (){
+	if(raiz->getHijoIzquierda()->altura() < raiz->altura()-1 &&
+		 raiz->getHijoIzquierda()->getHijoIzquierda()->altura() > raiz->getHijoIzquierda()->getHijoDerecha()->altura()){
+		Nodo * aux = raiz->getHijoIzquierda();
+		raiz->setHijoIzquierda(raiz->getHijoIzquierda()->getHijoDerecha());
+		aux->setHijoDerecha(raiz);
+		this->setRaiz(aux);
+		cout<<"Caso 1"<endl;
+	}
+}
+
+void caso2 (){
+	if(raiz->getHijoIzquierda()->altura() < raiz->altura()-1 &&
+		 raiz->getHijoDerecha()->getHijoDerecha()->altura() > raiz->getHijoDerecha()->getHijoIzquierda()->altura()){	
+		Nodo * aux = raiz->getHijoDerecha();
+		raiz->setHijoDerecha(raiz->getHijoDerecha()->getHijoIzquierda());
+		aux->setHijoIzquierda(raiz);
+		this->setRaiz(aux);
+		cout<<"Caso 2"<endl;
+	}
+}
+
+void caso3 (){
+	if(raiz->getHijoIzquierda()->altura() > raiz->getHijoDerecha()->altura()+1 &&
+		 raiz->getHijoIzquierda()->getHijoDerecha()->altura() > raiz->getHijoIzquierda()->getHijoIzquierda()->altura()){
+		Nodo * aux = raiz->getHijoDerecha();
+		aux->setHijoDerecha(raiz);
+		aux->getHijoDerecha()->setHijoIzquierda(raiz->getHijoIzquierda()->getHijoDerecha()->getHijoDerecha());
+		raiz->getHijoIzquierda()->setHijoDerecha(raiz->getHijoIzquierda()->getHijoDerecha()->getHijoIzquierda());
+		aux->setHijoIzquierda(raiz->getHijoIzquierda());
+		this->setRaiz(aux);
+		cout<<"Caso 3"<endl;
+	}
+}
+
+void caso4 (){
+	if(raiz->getHijoIzquierda()->altura() > raiz->getHijoDerecha()->altura()+1 &&
+		 raiz->getHijoDerecha()->getHijoIzquierda()->altura() > raiz->getHijoDerecha()->getHijoDerecha()->altura()){
+		Nodo * aux = raiz->getHijoDerecha()->getHijoIzquierda();
+		aux->setHijoIzquierda(raiz);
+		aux->getHijoIzquierda()->setHijoDerecha(raiz->getHijoDerecha()->getHijoIzquierda()->getHijoIzquierda());
+		raiz->getHijoDerecha()->setHijoIzquierda(raiz->getHijoDerecha()->getHijoIzquierda()->getHijoDerecha());
+		aux->setHijoDerecha(raiz->getHijoDerecha());
+		this->setRaiz(aux);
+		cout<<"Caso 4"<endl;
+	}	
 }
